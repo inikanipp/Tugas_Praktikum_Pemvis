dcb43a1cba6fc59434dd1d816bce5e904137eedc
inikanipp <pengumpulanlomba@gmail.com>
2024-11-21 19:08:37 +0700

    19.08

---
+++ modul5/projectAkhir/src/projectakhir/koneksi.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectakhir;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+
+/**
+ *
+ * @author acer
+ */
+public class koneksi {
+    private static java.sql.Connection koneksi;
+    
+    public static java.sql.Connection getKoneksi(){
+            try{
+                String url = "jdbc:mysql://localhost:3306/db_projectakhir";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Koneksi Berhasil");
+            }catch (Exception e){
+                System.err.println("Koneksi Gagal : " + e.getMessage());
+            }
+        return koneksi;
+    }
+    
+    public static void main(String[] args) {
+        getKoneksi();
+    }
+}
---
+++ modul5/projectAkhir/src/projectakhir/username.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectakhir;
+
+/**
+ *
+ * @author Admin
+ */
+public class username {
+    private String username, password;
+    private int level;
+    
+    
+    protected String getUsername(){
+        return this.username;
+    }
+
+
+    protected int getLevel(){
+    return this.level;
+    }
+    
+    protected void setUsername(String username){
+        this.username = username;
+    }
+
+    protected void setLevel(int level){
+        this.level = level;
+    }
+    
+    
+}
+
+    
